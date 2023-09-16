96de0ea36608d19b13fe8b5adc15795ab85dad0c
wangkai <478742901@qq.com>
2023-09-16 19:41:13 +0800

    Chapter1 and Chapter2

---
+++ Chapter2/nogood.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, n2, n3;
+
+    n = 5;
+    n2 = n * n;
+    n3 = n * n * n;
+    printf("| n = %d | n2 = %d | n3 = %d |\n", n, n2, n3);
+
+    return 0;
+}
